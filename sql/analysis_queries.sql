83aefcdda85493bab2de3bf90293a43b1b4038d5
Amy Daniel <amymchargue@ymail.com>
2026-04-08 19:07:57 -0400

    Add contaminant frequency analysis query

--- sql/analysis_queries.sql
+++ sql/analysis_queries.sql
@@ -21,3 +21,16 @@ ON v.contaminant_code = c.contaminant_code
 GROUP BY v.contaminant_code, c.contaminant_name
 ORDER BY violation_count DESC
 LIMIT 10;
+
+-- Most common contaminants in Kentucky violations
+
+SELECT 
+    v.contaminant_code,
+    c.contaminant_name,
+    COUNT(*) AS violation_count
+FROM sdwa_violations v
+JOIN contaminants c
+ON v.contaminant_code = c.contaminant_code
+GROUP BY v.contaminant_code, c.contaminant_name
+ORDER BY violation_count DESC
+LIMIT 10;
